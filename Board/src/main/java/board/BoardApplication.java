6ebc8e1dc2d2e9b98dcdec6754f802d8f19bb39e
skyeo <skyeo@192.168.123.106>
2024-12-05 23:56:56 +0900

    commit

--- Board/src/main/java/board/BoardApplication.java
+++ Board/src/main/java/board/BoardApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 
-@SpringBootApplication
+@SpringBootApplication 
 @MapperScan(basePackages = "dao") // BoardDAO의 패키지 경로 설정
 @ComponentScan(basePackages = {"board", "controller", "service", "dao", "dto", "configuration", "exceptionHandle", "constant"}) 
 public class BoardApplication {
